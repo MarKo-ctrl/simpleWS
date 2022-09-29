223212ec3da2b590467c48612643efd9b60b696c
Marios Kontis <kontism@gmail.com>
2022-09-29 12:45:40 +0100

    test sending group message

--- __tests__/createWSserver.test.js
+++ __tests__/createWSserver.test.js
@@ -63,5 +63,54 @@ describe("WebSocket Server", () => {
     expect(messages3[0]).toBe(testMessage.value);
   });
 
-  
+  it ("When given a MESSAGE_GROUP message, the server echoes the message it receives to everyone in the specified group", async () => {
+    const testMessage = "This is a test message";
+    const creationMessage = { type: "CREATE_GROUP", value: "TEST_GROUP" };
+
+    // Create test clients
+    const [client1, messages1] = await createSocketClient(port);
+    const [client2, messages2] = await createSocketClient(port, 2);
+    const [client3, messages3] = await createSocketClient(port);
+
+    // Setup test clients to send messages and close in the right order
+    client1.on("message", (data) => {
+      
+      if (JSON.parse(data) === creationMessage.value) {
+        const joinMessage = { type: "JOIN_GROUP", value: JSON.parse(data) };
+        const groupMessage = {
+          type: "MESSAGE_GROUP",
+          value: { groupName: JSON.parse(data), groupMessage: testMessage },
+        };
+        client2.send(JSON.stringify(joinMessage));
+        client2.send(JSON.stringify(groupMessage));
+      }
+    });
+
+    client2.on("close", () => {
+      client1.close();
+      client3.close();
+    });
+
+    // Send client message
+    client1.send(JSON.stringify(creationMessage));    
+
+    // Perform assertions on the responses
+    await waitSocketState(client1, client1.CLOSED);
+    await waitSocketState(client2, client2.CLOSED);
+    await waitSocketState(client3, client3.CLOSED);
+
+    const [group1, message1] = messages1;
+    const [group2, message2] = messages2;
+
+    // Both client1 and client2 should have joined the same group.
+    expect(group1).toBe(creationMessage.value);
+    expect(group2).toBe(creationMessage.value);
+
+    // Both client1 and client2 should have received the group message.
+    expect(message1).toBe(testMessage);
+    expect(message2).toBe(testMessage);
+
+    // client3 should have received no messages
+    expect(messages3.length).toBe(0);
+  });
 });
--- createWSserver.js
+++ createWSserver.js
@@ -1,5 +1,7 @@
 const { WebSocket } = require('ws');
 
+const groupNames = [];
+
 exports.createWebSocketServer = (server) => {
   const wss = new WebSocket.Server({ server });
 
@@ -9,12 +11,44 @@ exports.createWebSocketServer = (server) => {
 
       if (data.type === 'ECHO') {
         webSocket.send(JSON.stringify(data.value))
-      } else if (data.type === 'ECHO_TO_ALL') {
-        wss.clients.forEach((ws) => ws.send(JSON.stringify(data.value)));
-      } else {
+      }
+      else if (data.type === 'ECHO_TIMES_3') {
         for (let i = 1; i <= Number.parseInt(data.type.slice(-1)); i++) {
           webSocket.send(JSON.stringify(data.value))
         }
+      }
+      else if (data.type === 'ECHO_TO_ALL') {
+        wss.clients.forEach((ws) => ws.send(JSON.stringify(data.value)));
+      }
+      else if (data.type === 'CREATE_GROUP') {
+        const groupName = data.value;
+
+        if (!groupNames.find((gn) => gn === groupName)) {
+          groupNames.push(groupName);
+          webSocket.groupName = groupName;
+          webSocket.send(JSON.stringify(groupName));
+        }
+        else {
+          webSocket.send(JSON.stringify('GROUP_UNAVAILABLE'));
+        }
+      }
+      else if (data.type === 'JOIN_GROUP') {
+        const groupName = data.value;
+
+        if (!groupNames.find((gn) => gn === groupName)) {
+          webSocket.send(JSON.stringify('GROUP_UNAVAILABLE'));
+        }
+        else {
+          webSocket.groupName = groupName;
+          webSocket.send(JSON.stringify(groupName));
+        }
+      }
+      else if (data.type === 'MESSAGE_GROUP') {
+        const { groupName, groupMessage } = data.value;
+        if (webSocket.groupName !== groupName);
+        wss.clients.forEach((ws) => {
+          if (ws.groupName === groupName) ws.send(JSON.stringify(groupMessage));
+        });
       }
     })
   })
